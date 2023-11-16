85298f8784a677597b382fa8b3038217233acc49
Jahas <jahaspty1@gmail.com>
2023-11-16 12:52:14 +0530

    db atlas change

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect('mongodb://127.0.0.1:27017/propertify', {
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
