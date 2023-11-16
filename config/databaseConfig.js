58dd5a71942ba4f9ad289f4c81bb8122df06964f
Jahas <jahaspty1@gmail.com>
2023-11-16 16:37:48 +0530

    Logo Changed For Privacypolicy

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-// mongoose.connect(process.env.MONGO_URL, {
+// mongoose.connect('mongodb://127.0.0.1:27017/propertify', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // })
 
-mongoose.connect('mongodb://127.0.0.1:27017/propertify', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
